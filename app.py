64f0c77614243b9f83203ca29adb87bc1a9b4eef
Alexey Gladyshev <alexey0gladyshev@gmail.com>
2017-05-26 16:33:40 +0300

    add wtforms and flashing

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_script import Manager
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 manager = Manager(app)
 moment = Moment(app)
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
 
 @app.errorhandler(401)
 def unauthorized(e):
@@ -23,9 +32,16 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+	form = NameForm()
+	if form.validate_on_submit():
+	    old_name = session.get('name')
+	    if old_name is not None and old_name != form.name.data:
+	        flash('Looks like you have changed your name!')
+	    session['name'] = form.name.data
+	    return redirect(url_for('index'))
+	return render_template('index.html', form=form, name=session.get('name'))
 
 
 if __name__ == '__main__':
